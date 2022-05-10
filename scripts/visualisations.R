978456148f54494e9e59c31f2349dcf2514672e7
Hardip Patel <patelhardip@gmail.com>
2022-05-11 09:43:30 +1000

    Update visualisations.R

--- scripts/visualisations.R
+++ scripts/visualisations.R
@@ -33,3 +33,45 @@ kp <- plotKaryotype(genome = platypus.genome, chromosomes = chrorder, main = "nS
 kpAddBaseNumbers(kp, tick.dist = 5e6)
 kpLines(kp, chr=relernn$chrname, x=relernn$start, y = relernn$nSites, ymin = 0, ymax = max(relernn$nSites))
 
+
+
+
+tmp <- list()
+
+x <- read_delim("Downloads/PlatypusWGS.20unrelatedfemalesamples.GCF_004115215.2_mOrnAna1.pri.v4.bcftools.PREDICT.BSCORRECTED.txt", delim="\t")
+x$stype <- "females"
+tmp[[1]] <- x
+x <- read_delim("Downloads/PlatypusWGS.44unrelatedsamples.GCF_004115215.2_mOrnAna1.pri.v4.bcftools.PREDICT.BSCORRECTED.txt", delim="\t")
+x$stype <- "all"
+tmp[[2]] <- x
+x <- read_delim("Downloads/PlatypusWGS.27unrelatedmalesamples.GCF_004115215.2_mOrnAna1.pri.v4.bcftools.PREDICT.BSCORRECTED.txt", delim="\t")
+x$stype <- "males"
+tmp[[3]] <- x
+
+tmp <- bind_rows(tmp)
+cname <- data.frame(chrom = pull(x, chrom) %>% unique(), cname = paste("chr", c(1:21), sep = ""))
+tmp <- left_join(tmp,cname)
+c <- unique(tmp$cname)
+
+tmp %>% ggplot(aes(x=start, y=recombRate, color = stype)) + 
+  geom_point(alpha=0.5,size=0.6) + 
+  facet_wrap(~factor(cname, levels=c), ncol=2) + 
+  theme_bw() +
+  theme(legend.text = element_text(size=20)) + 
+  xlab("Position") + ylab("crossovers / base")
+
+
+for (i in 1:length(c)) {
+  pdf(paste("Downloads/",c[i],".pdf",sep = ""), width = 10, height = 3)
+  g <- filter(tmp, cname == c[i]) %>% 
+    ggplot(aes(x=start, y=recombRate, color = stype)) + 
+    geom_point(alpha = 0.5)
+  print(g)
+  dev.off()
+}
+
+
+
+
+
+
